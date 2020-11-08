ce7cd39efc0346466b77547cad8d49cc01964060
maimaitiaziguli <arzuyum507@gmail.com>
2020-11-07 22:52:25 -0800

    Initial commit

---
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -0,0 +1,47 @@
+package com.vytrack.step_definitions;
+
+
+import com.vytrack.pages.LoginPage;
+import com.vytrack.utils.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginStepDefinitions {
+    LoginPage loginPage = new LoginPage();
+
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+        Driver.getDriver().get("http://qa1.vytrack.com");
+    }
+
+    @When("user logs in")
+    public void user_logs_in() throws InterruptedException {
+        loginPage.login();
+        Thread.sleep(3000);
+    }
+
+    @Then("user should see dashboard page")
+    public void user_should_see_dashboard_page() {
+        String expected = "Dashboard";
+        String actual = loginPage.getPageSubTitleText().trim();
+
+        Assert.assertEquals("Title is not correct!", expected, actual);
+        System.out.println("I see the Dashboard page!");
+        Driver.closeDriver();
+    }
+
+    //When user logs in as a "driver" --> public void user_logs_in_as_a(String string) -> loginPage.login(string); -> public void login(String role) { if role == "" do this..}
+    //When user logs in as a "sales manager"
+    //When user logs in as a "store manager"
+    @When("user logs in as a {string}")
+    public void user_logs_in_as_a(String string) {
+        loginPage.login(string);
+    }
+}
---
+++ src/test/resources/features/Login.feature
@@ -0,0 +1,17 @@
+Feature: As user I want to be able to login under different roles
+# this is a comment
+
+  Scenario: Login as a sales manger
+    Given user is on the login page
+    When user logs in
+    Then user should see dashboard page
+
+  @parametrized_test
+  Scenario: Parametrized login
+    Given user is on the login page
+    When user logs in as a "store manager"
+    Then user should see dashboard page
+
+#"driver" - is a parameter. "" allows to do test parametrization which helps to re-use test steps
+
+
